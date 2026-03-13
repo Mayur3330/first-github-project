01ccfc0ab81e083505f9810c2c49467e506d1259
Mayur <singhmayur495@gmail.com>
2026-03-13 08:37:06 +0530

    Made new codes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import os
 
+#I just want to check during merging if any conflict happens how to handle
 print("I am Mayur")
 
 print("I am not Mayur")
+
+print(os.getcwd)
